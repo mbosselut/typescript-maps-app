f27b7666f78a8d75f0b7bcb3df35b796e42acfdc
mbosselut <duchmol7@hotmail.com>
2020-01-17 15:16:11 +0100

    Add user and company markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,10 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  }
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -9,5 +16,15 @@ export class CustomMap {
         lng: 0
       }
     });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
   }
 }
